bc64e01a58009628ba1f70133444c87bfabeceaa
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-12-05 18:00:56 +0200

    Add hooks and utils

---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const useForm = (submitHandler, initialValues) => {
+    const [values, setValues] = useState(initialValues);
+
+    // useEffect(() => {
+    //     setValues(initialValues);
+    // }, [initialValues])
+
+    const onChange = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        submitHandler(values);
+    };
+
+    return {
+        values,
+        onChange,
+        onSubmit,
+    }
+};
+
+export default useForm;
---
+++ client/src/lib/request.js
@@ -0,0 +1,43 @@
+const buildOptions = (data) => {
+    const options = {};
+
+    if (data) {
+        options.body = JSON.stringify(data);
+        options.headers = {
+            'content-type': 'application/json'
+        };
+    }
+
+    // const token = localStorage.getItem('accessToken');
+
+    // if (token) {
+    //     options.headers = {
+    //         ...options.headers,
+    //         'X-Authorization': token
+    //     }
+    // }
+
+    return options;
+}
+
+const request = async (method, url, data) => {
+    console.log(data);
+    const response = await fetch(url, {
+        ...buildOptions(data),
+        method,
+    });
+
+    const result = response.json();
+
+    if (!response.ok) {
+        throw result;
+    }
+
+    return result;
+}
+
+export const get = request.bind(null, 'GET');
+export const post = request.bind(null, 'POST');
+export const put = request.bind(null, 'PUT');
+export const remove = request.bind(null, 'DELETE');
+export const patch = request.bind(null, 'PATCH');
