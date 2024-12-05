7835be7dd054e47646d7d6134409c55037572d55
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-12-05 17:58:14 +0200

    Add new packages and main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import ErrorBoundary from './components/errorBoundary/ErrorBoundary';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ErrorBoundary>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ErrorBoundary>
 )
